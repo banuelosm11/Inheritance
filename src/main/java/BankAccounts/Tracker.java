9e09273007c5309aecd58061860602d6917b8de4
Aurora Banuelos <aurorabanuelos@zipcoder5.local>
2017-05-03 11:43:16 -0400

    finished tracker

--- src/main/java/BankAccounts/Tracker.java
+++ src/main/java/BankAccounts/Tracker.java
@@ -4,4 +4,27 @@ package BankAccounts;
  * Created by aurorabanuelos on 5/3/17.
  */
 public class Tracker {
+
+    private double updatedBalance;
+
+    public void withdrawal(double requestAmount, boolean requestType, Account account1){
+
+        if (requestType == true && (requestAmount < account1.getCurrentBalance())) {
+
+            updatedBalance = account1.getCurrentBalance() - requestAmount;
+
+            account1.setCurrentBalance(updatedBalance);
+
+        }
+        else if (requestType == false){
+
+            updatedBalance = account1.getCurrentBalance() + requestAmount;
+
+            account1.setCurrentBalance(updatedBalance);
+        }
+
+        else{
+            System.out.println("insufficient funds");
+        }
+    }
 }
--- src/test/java/BankAccountsTest/TrackerTest.java
+++ src/test/java/BankAccountsTest/TrackerTest.java
@@ -1,5 +1,8 @@
 package BankAccountsTest;
 
+import BankAccounts.CheckingAccount;
+import BankAccounts.Tracker;
+import org.junit.Assert;
 import org.junit.Test;
 
 /**
@@ -8,5 +11,20 @@ import org.junit.Test;
 public class TrackerTest {
 
     @Test
-    public vositAmount(requ
+    public void withdrawalTest(){
+        //Given:
+        double requestAmount = 20.00;
+        boolean isWithdrawal = true;
+        CheckingAccount account1 = new CheckingAccount("Aurora", "Checking", 100.00 );
+        Tracker newRequest = new Tracker();
+
+        //When:
+        newRequest.withdrawal(requestAmount, isWithdrawal, account1);
+        double actualResult = account1.getCurrentBalance();
+        double expectedResult = 80.00;
+
+        //Then:
+        Assert.assertEquals(expectedResult, actualResult, 0);
+
+    }
 }
