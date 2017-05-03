f554947e759c170cf5e58f6e8a0ed5ade8b9fdf4
Aurora Banuelos <aurorabanuelos@zipcoder5.local>
2017-05-02 20:20:33 -0400

    added product

--- src/main/java/ClassManager/Product.java
+++ src/main/java/ClassManager/Product.java
@@ -4,4 +4,44 @@ package ClassManager;
  * Created by aurorabanuelos on 5/2/17.
  */
 public class Product {
+
+    private String id;
+    private double price;
+    private int quantity;
+
+
+
+    public Product (String id, double price, int quantity){
+        this.id = id;
+        this.price = price;
+        this.quantity = quantity;
+    }
+
+
+
+    public String getId(){
+        return id;
+    }
+
+    public double getPrice() {
+        return price;
+    }
+
+    public int getQuantity() {
+        return quantity;
+    }
+
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public void setPrice(double price){
+        this.price = price;
+    }
+
+    public void setQuantity(int quantity){
+        this.quantity = quantity;
+    }
+
 }
